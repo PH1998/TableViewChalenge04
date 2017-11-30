e0c2ddd1d367878d1d76a1b409fc0beab1aed60c
Senac Playgrounds <senacipad9@mail.com>
2017-11-30 14:05:50 -0200

    Initial Commit

---
+++ TableViewChalenge04/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewChalenge04
+//
+//  Created by PEDRO HENRIQUE PEREIRA ELIAS DOS SANTOS on 30/11/17.
+//  Copyright © 2017 PEDRO HENRIQUE PEREIRA ELIAS DOS SANTOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
